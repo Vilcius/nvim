9dba810b96671b144cf182d7d32c0110abd8b12b
Vilcuis <vilcius@protonmail.com>
2024-01-25 21:14:08 -0500

    Added Ultisnips, disabled treesitter for latex, a copilot thing.

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ignore_install = { "latex" },
+    highlight = {
+      disable = { "latex" },
+    },
+  }
+}
--- lua/plugins/vim-jukit.lua
+++ lua/plugins/vim-jukit.lua
@@ -1,4 +1,6 @@
-local wk = require("which-key")
+-- local wk = require("which-key")
+-- FIX: Make sure the keymaps are defined correctly.
+-- TODO: Set localleader to "\" and redefine the keymaps.
 
 vim.g.python3_host_prog = '/usr/bin/python3'
 vim.g.jukit_mappings_ext_enabled = 'py'
@@ -15,16 +17,20 @@ vim.api.nvim_set_hl(0, "jukit_cellmarker_colors", { fg = "#414868", bg = "#41486
 vim.api.nvim_set_hl(0, "jukit_textcell_bg_colors", { bg = "#1e2030", fg = "#c4a7e7" })
 
 -- Jukit Python remap
-wk.register({
-    ["<C-PageDown>"] = { "<cmd>call jukit#cells#jump_to_next_cell()<CR>zz", "Jump to next cell" },
-    ["<C-PageUp>"] = { "<cmd>call jukit#cells#jump_to_previous_cell()<CR>zz", "Jump to previous cell" },
-    -- ["<leader><space>"] = { "<cmd>call jukit#send#section(0)<cr>zz", "Run cell" },
-    -- ["<leader><CR>"] = { "<cmd>call jukit#send#line()<cr>zz", "Run line" },
-    -- ["<leader><S-leader>"] = { "<cmd>call jukit#send#section(0)<bar>jukit#cells#jump_to_next_cell()<bar>jukit#cells#jump_to_next_cell()<cr>zz", "Run cell and move to next" },
-  },
-  {
-    mode = "n",
-  })
+-- wk.register({
+--     ["<C-PageDown>"] = { "<cmd>call jukit#cells#jump_to_next_cell()<CR>zz", "Jump to next cell" },
+--     ["<C-PageUp>"] = { "<cmd>call jukit#cells#jump_to_previous_cell()<CR>zz", "Jump to previous cell" },
+--     -- ["<leader><space>"] = { "<cmd>call jukit#send#section(0)<cr>zz", "Run cell" },
+--     -- ["<leader><CR>"] = { "<cmd>call jukit#send#line()<cr>zz", "Run line" },
+--     -- ["<leader><S-leader>"] = { "<cmd>call jukit#send#section(0)<bar>jukit#cells#jump_to_next_cell()<bar>jukit#cells#jump_to_next_cell()<cr>zz", "Run cell and move to next" },
+--   },
+--   {
+--     mode = "n",
+--   })
+
+vim.keymap.set('n', '<C-PageDown>', "<cmd>call jukit#cells#jump_to_next_cell()<CR>zz", { desc = "Jump to next cell" })
+vim.keymap.set('n', '<C-PageUp>', "<cmd>call jukit#cells#jump_to_previous_cell()<CR>zz",
+  { desc = "Jump to previous cell" })
 
 return {
   "luk400/vim-jukit",
